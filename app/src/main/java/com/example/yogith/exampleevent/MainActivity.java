c76779fb85a280db7da664cfccb6a50c3fc06ee7
YogithTeegala <yogithteegala@gmail.com>
2017-06-25 00:08:59 +0530

    Skeleton commit

--- app/src/main/java/com/example/yogith/exampleevent/MainActivity.java
+++ app/src/main/java/com/example/yogith/exampleevent/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.yogith.exampleevent;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void addButton(View view) {
+        Intent intent = new Intent(this, AddEventActivity.class);
+        startActivity(intent);
+    }
+    public void viewButton(View view) {
+        Intent intent = new Intent(this, ViewEventActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }

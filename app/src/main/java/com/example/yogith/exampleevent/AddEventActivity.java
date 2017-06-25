3b54f19029019a189ac8256ee2c724d777a9a312
YogithTeegala <yogithteegala@gmail.com>
2017-06-26 01:35:25 +0530

    DatePicker commit

--- app/src/main/java/com/example/yogith/exampleevent/AddEventActivity.java
+++ app/src/main/java/com/example/yogith/exampleevent/AddEventActivity.java
@@ -1,13 +1,61 @@
 package com.example.yogith.exampleevent;
 
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    public TextView date;
+    private DatePickerDialog.OnDateSetListener OnDateSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        date = (TextView) findViewById(R.id.Ddate);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+
+                int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+
+
+                    DatePickerDialog dialog = new DatePickerDialog(
+                            AddEventActivity.this,
+                            OnDateSet, year, month, dayOfMonth);
+                    //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.show();
+
+            }
+        });
+
+        OnDateSet = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String inputDate = day + "/" + month + "/" + year;
+                date.setText(inputDate);
+            }
+        };
     }
 }

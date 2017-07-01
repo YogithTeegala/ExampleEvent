613475e8d1ab0da8be6914bdd089f454e0c0d6fc
YogithTeegala <yogithteegala@gmail.com>
2017-07-01 10:30:04 +0530

    DatePicker commit

--- app/src/main/java/com/example/yogith/exampleevent/AddEventActivity.java
+++ app/src/main/java/com/example/yogith/exampleevent/AddEventActivity.java
@@ -2,6 +2,7 @@ package com.example.yogith.exampleevent;
 
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.ParseException;
@@ -22,39 +24,53 @@ import java.util.Date;
 
 public class AddEventActivity extends AppCompatActivity {
 
-    public TextView date;
-    private DatePickerDialog.OnDateSetListener OnDateSet;
+    public TextView date, time;
+    private DatePickerDialog.OnDateSetListener onDateSet;
+    private TimePickerDialog.OnTimeSetListener onTimeSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
-        date = (TextView) findViewById(R.id.Ddate);
+
+        date = (TextView) findViewById(R.id.dateDisplay);
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar cal = Calendar.getInstance();
-
                 int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
                 int month = cal.get(Calendar.MONTH);
                 int year = cal.get(Calendar.YEAR);
-
-
-                    DatePickerDialog dialog = new DatePickerDialog(
-                            AddEventActivity.this,
-                            OnDateSet, year, month, dayOfMonth);
-                    //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    dialog.show();
-
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddEventActivity.this, onDateSet, year, month, dayOfMonth);
+                //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
         });
-
-        OnDateSet = new DatePickerDialog.OnDateSetListener() {
+        onDateSet = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 month = month + 1;
                 String inputDate = day + "/" + month + "/" + year;
                 date.setText(inputDate);
+            }
+        };
+        time = (TextView) findViewById(R.id.timeDisplay);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int min = cal.get(Calendar.MINUTE);
+                TimePickerDialog dialog = new TimePickerDialog(AddEventActivity.this, onTimeSet, hour, min, false);
+                dialog.show();
+            }
+        });
+        onTimeSet = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String inputTime = hourOfDay + ":" + minute;
+                time.setText(inputTime);
             }
         };
     }

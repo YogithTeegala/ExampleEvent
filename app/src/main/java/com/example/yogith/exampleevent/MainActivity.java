7a836bbb3b3f1aa1f6d337cf16d987b18653f250
YogithTeegala <yogithteegala@gmail.com>
2017-07-12 22:25:56 +0530

    Sample notification

--- app/src/main/java/com/example/yogith/exampleevent/MainActivity.java
+++ app/src/main/java/com/example/yogith/exampleevent/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.yogith.exampleevent;
 
-import android.support.annotation.IdRes;
+
+import android.app.PendingIntent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-import android.view.View;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends AppCompatActivity {
     //DBHelper dBHelper;
@@ -18,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dBHelper = new DBHelper(this);
+        this.addNotification();
     }
     public void addButton(View view) {
         Intent intent = new Intent(this, AddEventActivity.class);
@@ -27,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
     public void viewButton(View view) {
         Intent intent = new Intent(this, ViewEventActivity.class);
         startActivity(intent);
+    }
+    public void addNotification(){
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification)
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
     }
 
 
